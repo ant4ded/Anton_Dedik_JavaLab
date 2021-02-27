7ef9e0f963e2c81bee67ac87400aba4b8e789dd4
ant4ded <ant4ded@gmail.com>
2021-02-27 17:47:24 +0300

    Rename package

--- Task2/gift_certificates_system/core/src/main/java/com/epam/esm/core/Application.java
+++ Task2/gift_certificates_system/core/src/main/java/com/epam/esm/core/Application.java
@@ -1,6 +1,6 @@
 package com.epam.esm.core;
 
-import com.epam.esm.data_access.conf.RepositoryConfiguration;
+import com.epam.esm.data_access.conf.DataAccessConfiguration;
 import com.epam.esm.core.conf.ServiceConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class Application {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(ServiceConfiguration.class,
-                RepositoryConfiguration.class);
+                DataAccessConfiguration.class);
         for (String beanName : context.getBeanDefinitionNames()) {
             System.out.println(beanName);
         }
---
+++ Task2/gift_certificates_system/data_access/src/main/java/com/epam/esm/data_access/repository/impl/GiftTagRepositoryImpl.java
@@ -0,0 +1,7 @@
+package com.epam.esm.data_access.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class GiftTagRepository {
+}
