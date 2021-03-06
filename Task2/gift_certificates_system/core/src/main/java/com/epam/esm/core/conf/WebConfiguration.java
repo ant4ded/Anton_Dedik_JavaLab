95c6981a389d41e26b393cb3bc08fa413627e3cb
ant4ded <ant4ded@gmail.com>
2021-03-07 01:55:46 +0300

    Run as web

--- Task2/gift_certificates_system/core/src/main/java/com/epam/esm/core/conf/WebConfiguration.java
+++ Task2/gift_certificates_system/core/src/main/java/com/epam/esm/core/conf/WebConfiguration.java
@@ -4,10 +4,13 @@ import com.epam.esm.data_access.conf.DataAccessConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 @Import(DataAccessConfiguration.class)
 @ComponentScan(basePackages = "com.epam.esm.core")
-public class ServiceConfiguration {
+public class ServiceConfiguration implements WebMvcConfigurer {
 
 }
