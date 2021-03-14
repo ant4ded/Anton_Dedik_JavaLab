3372fd645f30c8ffb42413f5dd776f2d57d7bd9d
ant4ded <ant4ded@gmail.com>
2021-03-14 17:33:15 +0300

    Rewrite with spring boot

--- Task3/gift_certificates_system/core/src/main/java/com/epam/esm/core/Application.java
+++ Task3/gift_certificates_system/core/src/main/java/com/epam/esm/core/Application.java
@@ -1,34 +1,28 @@
 package com.epam.esm.core;
 
-import com.epam.esm.core.conf.WebConfiguration;
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.filter.CharacterEncodingFilter;
-import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.servlet.DispatcherType;
-import javax.servlet.FilterRegistration;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletRegistration;
-import java.nio.charset.StandardCharsets;
-import java.util.EnumSet;
-
-@SuppressWarnings("NullableProblems")
-public class Application implements WebApplicationInitializer {
-    @Override
-    public void onStartup(ServletContext servletContext) {
-        AnnotationConfigWebApplicationContext webCtx = new AnnotationConfigWebApplicationContext();
-        webCtx.register(WebConfiguration.class);
-        webCtx.setServletContext(servletContext);
-
-        ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(webCtx));
-        servlet.setLoadOnStartup(1);
-        servlet.addMapping("/");
-        servlet.setAsyncSupported(true);
-
-        FilterRegistration.Dynamic filter = servletContext.addFilter("encodingFilter",
-                new CharacterEncodingFilter(StandardCharsets.UTF_8.name(), true, true));
-        filter.setAsyncSupported(true);
-        filter.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
     }
+
+//    @Override
+//    public void onStartup(ServletContext servletContext) {
+//        AnnotationConfigWebApplicationContext webCtx = new AnnotationConfigWebApplicationContext();
+//        webCtx.register(WebConfiguration.class);
+//        webCtx.setServletContext(servletContext);
+//
+//        ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(webCtx));
+//        servlet.setLoadOnStartup(1);
+//        servlet.addMapping("/");
+//        servlet.setAsyncSupported(true);
+//
+//        FilterRegistration.Dynamic filter = servletContext.addFilter("encodingFilter",
+//                new CharacterEncodingFilter(StandardCharsets.UTF_8.name(), true, true));
+//        filter.setAsyncSupported(true);
+//        filter.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+//    }
 }
